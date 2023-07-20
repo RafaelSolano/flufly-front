9446d3719af68e15f039440a4bab6cce8e8e0549
Rafael Solano <rafaelsolanol007@gmail.com>
2023-07-20 18:32:35 -0500

    Se crea la pagina About falt adar estilos al header

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
-
+import './about.css'
 function about() {
   return (
-    <>
-      <h1>Soy el About </h1>
-    </>
+    <section className='about_page_container'>
+      <div>
+        <h2>
+          Conoce Acerca de nosotros
+        </h2>
+      </div>
+      <section className='about-container'>
+        <figure>
+          <img className='about_img' src="https://img.freepik.com/fotos-premium/dos-perros-gato-mirando-camara_748076-85.jpg?w=360" alt="perros y gatos uno arriba de otro" />
+        </figure>
+        <div className='about__text-container'>
+          <section className='about__section-txt'>
+            <h3>Historia</h3>
+            <p>Somos la tienda en línea de mascotas con mayor variedad de productos y servicios a domicilio en Bogotá con más de 2.000 productos como: alimentos, dietas naturales, snacks, medicinas, higiene y accesorios.
+
+              Para Fluffy, la salud y bienestar de tu mascota es lo más importante. Nos esforzamos día a día por dar la mejor experiencia y servicio a los pet lovers y para que a tus peluditos no les falte nada para su cuidado y salud.</p>
+          </section>
+
+          <section className='about__section-txt'>
+            <h3>Misión</h3>
+            <p>Brindar un servicio de excelente calidad y satisfacer las necesidades de sus mascotas, ofreciendo una excelente experiencia con los productos ofrecidos dentro de Fluffy  plataforma que se dedica a ofrecer lo mejor del mercado en pro del bienestar de tu mascota.</p>
+          </section>
+
+          <section className='about__section-txt'>
+            <h3>Visión</h3>
+            <p>Convertirnos en la plataforma virtual número uno para el cuidado de mascotas siendo reconocida por la calidad de nuestros productos y servicios.</p>
+          </section>
+
+        </div>
+      </section>
+
+    </section>
   )
 }
 
